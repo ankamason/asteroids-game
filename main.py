eeaae7ae21409ce4007e80ba5297dc1fb3f42e21
Anka Mason <ankamason@gmail.com>
2025-05-27 11:22:42 +0200

    Add FPS limiting and delta time calculation

--- main.py
+++ main.py
@@ -1,27 +1,40 @@
+
 import pygame
+
 from constants import *
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
+    
+    
     pygame.init()
-
+    
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    
+   
+    clock = pygame.time.Clock()
+    
+   
+    dt = 0
+    
+    # Game loop
     while True:
+        # Handle events (like closing the window)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return screen.fill("black")
-
+                return
+        
+        # Fill screen with black
+        screen.fill("black")
+        
+        # Refresh the display
         pygame.display.flip()
-
-
-
-
-
-
+        
+        # Limit to 60 FPS and calculate delta time
+        dt = clock.tick(60) / 1000
 
 
 
